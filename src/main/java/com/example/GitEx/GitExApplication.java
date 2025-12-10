e6b8265dc237c559207812cb6e60d18972f761a9
venkateshmsd34 <venkateshmsd34@gmail.com>
2025-12-10 10:38:42 +0530

    new commit

---
+++ src/main/java/com/example/GitEx/GitExApplication.java
@@ -0,0 +1,13 @@
+package com.example.GitEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GitEx/GitExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
